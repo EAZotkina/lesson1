eaeddc232c8ce592d1d82fb19904e5872c2cc021
“[firstname <zotkinae84@gmail.com>
2021-03-23 19:44:50 +0300

    Сделано задание 9 урока

---
+++ src/main/java/ru/geekbrains/lesson9/MyArrayDataException.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.lesson9;
+
+public class MyArrayDataException extends RuntimeException {
+    public MyArrayDataException(String message) {
+       super(message);
+    }
+}
+
---
+++ src/main/java/ru/geekbrains/lesson9/MyArraySizeException.java
@@ -0,0 +1,9 @@
+package ru.geekbrains.lesson9;
+
+public class MyArraySizeException extends RuntimeException {
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+}
+
+
